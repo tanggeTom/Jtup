24b246a9384769687fde1434639037c2ee931d8f
tangzc <123>
2023-09-28 23:18:13 +0800

    '更新jtup训练代码'

--- model/rf_rec_pre.py
+++ model/rf_rec_pre.py
@@ -2,6 +2,7 @@ import json
 import os
 from operator import attrgetter
 
+import psutil
 from imblearn.under_sampling import RandomUnderSampler
 from numpy import *
 import numpy as np
@@ -25,18 +26,16 @@ class Commit(object):
     def __str__(self):
         return "(" + self.file_name + ", " + str(self.time) + ")"
 
-    def __repr__(self):
-        return "(" + self.file_name + ", " + str(self.time) + ")"
-
-    def print(self):
-        print("123")
 
 
 # read json file and get features
 def read_json(filename, project):
     global positive_num, negative_num
     with open(filename, 'r', encoding='utf8') as fp:
-        json_data = json.load(fp)
+        try:
+            json_data = json.load(fp)
+        except:
+            print(filename)
         add_annotation_line = json_data['add_annotation_line']
         add_call_line = json_data['add_call_line']
         add_classname_line = json_data['add_classname_line']
@@ -59,12 +58,14 @@ def read_json(filename, project):
             "pre_cyclomatic_complexity"]
         last_cyclomatic_complexity = 0 if not json_data.get('last_cyclomatic_complexity') else json_data[
             "last_cyclomatic_complexity"]
-        pre_cyclomatic_complexity = str(pre_cyclomatic_complexity).replace(",", "")  # 存在1,234过千的数字
-        last_cyclomatic_complexity = str(last_cyclomatic_complexity).replace(",", "")
+        pre_cyclomatic_complexity = int(str(pre_cyclomatic_complexity).replace(",", ""))  # 存在1,234过千的数字
+        last_cyclomatic_complexity = int(str(last_cyclomatic_complexity).replace(",", ""))
         cyclomatic_complexity_diff = int(last_cyclomatic_complexity) - int(pre_cyclomatic_complexity)
-        pre_entropy = 0 if not json_data.get('pre_entropy') else json_data["pre_entropy"]
-        last_entropy = 0 if not json_data.get('last_entropy') else json_data["last_entropy"]
+        pre_entropy = 0 if not json_data.get('pre_entropy') else float(json_data["pre_entropy"])
+        last_entropy = 0 if not json_data.get('last_entropy') else float(json_data["last_entropy"])
         new_wv = json_data['new_wv']
+        new_wv_2 = json_data['new_wv_2']
+        new_wv_7 = json_data['new_wv_7']
         insert_num = 0 if not json_data.get('insert') else json_data["insert"]
         update_num = 0 if not json_data.get('update') else json_data["update"]
         move_num = 0 if not json_data.get('move') else json_data["move"]
@@ -86,14 +87,19 @@ def read_json(filename, project):
         modified_method_number_level = json_data["modified_method_number_level"]
         pro_modified_method_number_inclass = json_data["pro_modified_method_number_inclass"]
 
-        feature = [add_annotation_line, add_call_line, add_classname_line, add_condition_line, add_field_line,
+        feature = [
+            add_annotation_line, add_call_line, add_classname_line, add_condition_line, add_field_line,
                    add_import_line, add_packageid_line, add_parameter_line, add_return_line, del_annotation_line,
                    del_call_line, del_classname_line, del_condition_line, del_field_line, del_import_line,
                    del_packageid_line, del_parameter_line, del_return_line, insert_num, update_num, move_num,
-                   delete_num, clusters_num, actions_num, pre_cyclomatic_complexity, last_cyclomatic_complexity,
-                   cyclomatic_complexity_diff, pre_entropy, last_entropy, modified_code_lines_level,
+                   delete_num, clusters_num, actions_num,
+                   pre_cyclomatic_complexity, last_cyclomatic_complexity,
+                   cyclomatic_complexity_diff,
+            pre_entropy, last_entropy,
+                   modified_code_lines_level,
                    normalized_modified_code_lines, non_normalized_modified_code_lines, modified_method_number_level,
-                   pro_modified_method_number_inclass]
+                   pro_modified_method_number_inclass
+        ]
         feature.extend(new_wv)
         features.append(feature)
         try:
@@ -129,6 +135,7 @@ negative_num = 0
 project_dict = dict()
 project_train_dict = dict()
 if __name__ == '__main__':
+    start_time = time.time()
     projects = ['activemq', 'cloudstack', 'commons-math', 'flink', 'geode', 'james-project', 'logging-log4j2', 'storm',
                 'usergrid', 'zeppelin', 'biojava', 'jruby', 'jsoup',
                 "AntennaPod", "deeplearning4j", "junit4", "metrics", "OpenRefine", "storio", "wiremock"]
@@ -150,9 +157,8 @@ if __name__ == '__main__':
             target.append(commit.target)
             project_train_dict[commit.project] = project_train_dict.get(commit.project, 0) + 1
 
-    rgs = RandomForestClassifier(random_state=0, n_estimators=1000, max_features='sqrt')
-    rus = RandomUnderSampler(random_state=1)
-    features, target = rus.fit_resample(features, target)
+    rgs = RandomForestClassifier()
+    print(features[0],target[0])
     rgs = rgs.fit(features, target)
     predict = rgs.predict(features_test)
     print(len(predict))
@@ -160,3 +166,10 @@ if __name__ == '__main__':
     print('Recall:', recall_score(target_test, predict))
     print('F1-score:', f1_score(target_test, predict))
     print('Precision score:', precision_score(target_test, predict))
+    end_time = time.time()
+    runtime = end_time - start_time
+    print("running time:", runtime, "seconds")
+
+    process = psutil.Process()
+    memory_usage = process.memory_info().rss
+    print("memory usage:", memory_usage, "bytes")
